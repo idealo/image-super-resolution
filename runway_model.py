f8652b437f7102e498d71edc6ee11743ef6cc042
Anastasis Germanidis <agermanidis@gmail.com>
2019-05-30 18:49:58 -0400

    tfw you forget how to use your own SDK

--- runway_model.py
+++ runway_model.py
@@ -3,7 +3,7 @@ import numpy as np
 from ISR.models import RDN
 
 
-@runway.setup(options={'checkpoint': '2X_ArtifactCancelling'})
+@runway.setup(options={'checkpoint': runway.file(extension='.hdf5')})
 def setup(opts):
     rdn = RDN(arch_params={'C':6, 'D':20, 'G':64, 'G0':64, 'x':2})
     rdn.model.load_weights(opts['checkpoint'])
