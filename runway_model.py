9c4e76029121a8bfe4cbb3b8d2b869b803e6042c
Anastasis Germanidis <agermanidis@gmail.com>
2019-05-30 18:38:43 -0400

    port to runway

---
+++ runway_model.py
@@ -0,0 +1,19 @@
+import runway
+import numpy as np
+from ISR.models import RDN
+
+
+@runway.setup(options={'checkpoint': '2X_ArtifactCancelling'})
+def setup(opts):
+    rdn = RDN(arch_params={'C':6, 'D':20, 'G':64, 'G0':64, 'x':2})
+    rdn.model.load_weights(opts['checkpoint'])
+    return rdn
+    
+
+@runway.command('upscale', inputs={'image': runway.image}, outputs={'upscaled': runway.image})
+def upscale(rdn, inputs):
+    return rdn.predict(np.array(inputs['image']))
+
+
+if __name__ == '__main__':
+    runway.run(port=4231)
